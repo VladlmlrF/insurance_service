efb667ac75e8924708f56b2f9eef8f3854b397fb
VladlmlrF <vladimirfalin84@gmail.com>
2024-11-19 19:02:12 +0700

    feat: Created schemas

---
+++ insurance_app/api/api_v1/rates/schemas.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class RateSchema(BaseModel):
+    cargo_date: date
+    cargo_type: str
+    rate: float
+
+
+class InsuranceRequest(BaseModel):
+    cargo_date: date
+    cargo_type: str
+    rate: float
