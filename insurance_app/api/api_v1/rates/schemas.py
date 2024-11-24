ac4a7512d261a5e52b2101116377b2aea2221cb9
VladlmlrF <vladimirfalin84@gmail.com>
2024-11-24 13:15:31 +0700

    feat: Updated schemas

--- insurance_app/api/api_v1/rates/schemas.py
+++ insurance_app/api/api_v1/rates/schemas.py
@@ -3,9 +3,18 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class RateSchema(BaseModel):
+class RateBaseSchema(BaseModel):
     cargo_date: date
     cargo_type: str
+
+
+class RateUpdateSchema(BaseModel):
+    cargo_date: date | None = None
+    cargo_type: str | None = None
+    rate: float | None = None
+
+
+class RateSchema(RateBaseSchema):
     rate: float
 
 
